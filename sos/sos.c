8eae1660a51db856ea819fd358d468ae3201e457
Jonathan Cothran <jcrockville@gmail.com>
2021-10-14 11:19:01 -0400

    Function calls to DRY out code

--- sos/sos.c
+++ sos/sos.c
@@ -13,34 +13,30 @@ int main() {
 	while(true) {
 		if(ctrl) {
 			// ...
-			gpio_put(LED_PIN, GPIO_ON);
-            		sleep_ms(250);
-            		gpio_put(LED_PIN, GPIO_OFF);
-           		sleep_ms(60);
-            		gpio_put(LED_PIN, GPIO_ON);
-			sleep_ms(250);
-			gpio_put(LED_PIN, GPIO_OFF);
-			sleep_ms(60);
-			gpio_put(LED_PIN, GPIO_ON);
-			sleep_ms(250);
-			gpio_put(LED_PIN, GPIO_OFF);
-			sleep_ms(60);
+			s();
+            		s();
+			s();
 			ctrl = false;
 		}else {
 			// - - -
-			gpio_put(LED_PIN, GPIO_ON);
-			sleep_ms(750);
-			gpio_put(LED_PIN, GPIO_OFF);
-			sleep_ms(60);
-			gpio_put(LED_PIN, GPIO_ON);
-			sleep_ms(750);
-			gpio_put(LED_PIN, GPIO_OFF);
-			sleep_ms(60);
-			gpio_put(LED_PIN, GPIO_ON);
-			sleep_ms(750);
-			gpio_put(LED_PIN, GPIO_OFF);
-			sleep_ms(60);
+			o();
+			o();
+			o();
 			ctrl = true;
 		}
 	}
+}
+
+void s() {
+	gpio_put(LED_PIN, GPIO_ON);
+        sleep_ms(250);
+        gpio_put(LED_PIN, GPIO_OFF);
+        sleep_ms(60);
+}
+
+void o() {
+	gpio_put(LED_PIN, GPIO_ON);
+	sleep_ms(750);
+	gpio_put(LED_PIN, GPIO_OFF);
+	sleep_ms(60);
 }
